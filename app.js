75ff81a138a764ec78d1b4698af2ee99a6ddefaa
Facundo Ruiz Perez <80124560+facumruiz@users.noreply.github.com>
2021-08-09 15:01:19 -0300

    subo formulario con base
    Actualizacion de base por: https://console.firebase.google.com/u/1/project/inicio-logueo-one/firestore/data/~2F

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 /*DECLARO UNA FUNCION PARA MANDAR DATOS A BASE DE DATOS */
 
+const db = firebase.firestore();
+
+const todoForm = document.getElementById('todo_form');
+
+const create = (name, url, description) => {
+    db.collection('tasks').doc().set({
+        name,
+        url,
+        description
+    })
+}
+
+todoForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    const name = todoForm['todo_name'].value;
+    const url = todoForm['todo_url'].value;
+    const description = todoForm['todo_description'].value;
+
+    await create(name, url, description); // Llamo a mi función create
+
+    todoForm.reset(); // Reseteamos los campos
+});
+
+
 function funcion(){
     var email = document.getElementById('email').value;
     var password = document.getElementById('email').value;
